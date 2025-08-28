44f15dc65b39072352be07958e492df16b3fadf3
Ainz <143864591+Ber906@users.noreply.github.com>
2025-08-28 19:45:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const { HiAnime, HiAnimeError } = require('aniwatch');
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { HiAnime, HiAnimeError } from "aniwatch";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 5000;
